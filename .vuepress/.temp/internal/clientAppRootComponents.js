4979093c5d22910b83a372c478f7159c36a4d9aa
b3457m0d3 <perchance2dream86@gmail.com>
2021-06-29 15:51:35 -0400

    first

---
+++ .vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from '/home/b3457m0d3/Active Projects/vp/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
---
+++ .vuepress/.temp/internal/layoutComponents.js
@@ -0,0 +1,6 @@
+import { defineAsyncComponent } from 'vue'
+
+export const layoutComponents = {
+  "404": defineAsyncComponent(() => import("/home/b3457m0d3/Active Projects/vp/node_modules/@vuepress/theme-default/lib/client/layouts/404.vue")),
+  "Layout": defineAsyncComponent(() => import("/home/b3457m0d3/Active Projects/vp/node_modules/@vuepress/theme-default/lib/client/layouts/Layout.vue")),
+}
